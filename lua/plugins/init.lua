2e82a70651f5f4c3316a22fe12f0620c260d170c
Ryan Carter <ryan@frobozzelectric.org>
2025-03-24 11:01:05 -0700

    rest.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,4 +32,14 @@ return {
       vim.cmd([[colorscheme tokyonight-moon]])
     end,
   },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function (_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    }
+  },
 }
