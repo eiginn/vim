3890afec755e8e3693cf3cade4ee290056e6ffcc
Ryan Carter <ryan@frobozzelectric.org>
2025-11-20 09:36:13 -0800

    Add gofmt on write

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -10,6 +10,14 @@ return {
         ctagsbin = "gotags",
         ctagsargs = "-sort -silent",
       }
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+         require('go.format').gofmt()
+        end,
+        group = format_sync_grp,
+      })
     end,
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
